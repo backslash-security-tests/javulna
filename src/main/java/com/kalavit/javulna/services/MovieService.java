c5807d5b694f8267ce34910b1bb668cbc234558f
ofirabu42 <ofir@backslash.security>
2022-11-27 16:49:47 +0200

    feat: added sqli example that semgrep finds

--- src/main/java/com/kalavit/javulna/services/MovieService.java
+++ src/main/java/com/kalavit/javulna/services/MovieService.java
@@ -25,111 +25,77 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
-/**
- *
- * @author peti
- */
-@RequestMapping("/MovieService")
-public class MovieService {
-    
-    private static final Logger LOG = LoggerFactory.getLogger(MovieService.class);
-    
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-    
-    @Autowired
-    MovieAutoDao movieAutoDao;
 
+package servlets;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.io.FilenameUtils;
+
+
+@RequestMapping("/MovieService")
+public class Cls extends HttpServlet
+{
+    private static org.apache.log4j.Logger log = Logger.getLogger(Register.class);
+
+    // ruleid:find-sql-string-concatenation
     @PostMapping("/findMovie")
-    public List<MovieDto> findMovie(StringBuilder title, String description, String genre, String id) {
+    protected void danger(String ean) {
         Session session = this.sessionFactory.openSession();
 
-                String query = "select foo from bar where" + title + " limit 1";
-                try {
-                    PreparedStatement ps = session.connection().prepareStatement(query);
-                    ResultSet rs = ps.executeQuery();
-                    while (rs.next()) {
-                        Integer item = rs.getInt("foo");
-                    }
-                } catch (SQLException e) {
-                    logger.error("Error!", e);
-                } finally {
-                    session.close();
-                }
-
-        int conditions = 0;
-        StringBuilder sql = new StringBuilder("select description, title, genre, id from movie ");
-        if (StringUtils.hasText(title)) {
-            appendCondition(sql, conditions);
-            conditions++;
-            sql.append("title LIKE '%").append(title).append("%'");
-
-        }
-        if (StringUtils.hasText(description)) {
-            appendCondition(sql, conditions);
-            conditions++;
-            sql.append("description LIKE '%").append(description).append("%'");
-        }
-        if (StringUtils.hasText(genre)) {
-            appendCondition(sql, conditions);
-            conditions++;
-            sql.append("genre LIKE '%").append(genre).append("%'");
-        }
-        if (StringUtils.hasText(id)) {
-            appendCondition(sql, conditions);
-            conditions++;
-            sql.append("id = '").append(id).append("'");
-        }
-        LOG.debug(sql.toString());
-        List<MovieDto> users = this.jdbcTemplate.query(sql.toString(), new RowMapper<MovieDto>() {
-            @Override
-            public MovieDto mapRow(ResultSet rs, int rowNum) throws SQLException {
-                MovieDto ret = new MovieDto();
-                ret.setDescription(rs.getString("description"));
-                ret.setTitle(rs.getString("title"));
-                ret.setGenre(rs.getString("genre"));
-                ret.setId(rs.getString("id"));
-                return ret;
-            }
-        });
-
-        return users;
-    }
-
-    private void appendCondition(StringBuilder sb, int conditions) {
-        if (conditions == 0) {
-            sb.append(" where ");
-        } else {
-            sb.append(" and ");
-        }
-    }
-    
-    public Movie saveMovieFromXml(String xml){
+        String query = "select foo from bar where" + ean + " limit 1";
         try {
-            Movie m = new Movie();
-            DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            Document doc = db.parse(new ByteArrayInputStream(xml.getBytes("UTF-8")));
-            Element root = doc.getDocumentElement();
-            m.setTitle(getText(root, "title"));
-            m.setDescription(getText(root, "description"));
-            m.setGenre(getText(root, "genre"));
-            movieAutoDao.save(m);
-            return m;
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        } 
+            PreparedStatement ps = session.connection().prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Integer item = rs.getInt("foo");
+            }
+        } catch (SQLException e) {
+            logger.error("Error!", e);
+        } finally {
+            session.close();
+        }
+    }
+    // ruleid:find-sql-string-concatenation
+    protected void danger2(String biz) {
+        String query = "select foo from bar where" + biz + " limit 1";
+        Session session = this.sessionFactory.openSession();
+        try {
+            PreparedStatement ps = session.connection().prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Integer item = rs.getInt("foo");
+            }
+        } catch (SQLException e) {
+            logger.error("Error!", e);
+        } finally {
+            session.close();
+        }
     }
 
-    private String getText(Element el, String tagName) {
-        NodeList nl = el.getElementsByTagName(tagName);
-        if(nl != null && nl.getLength() >0){
-            NodeList children = nl.item(0).getChildNodes();
-            if(children != null && children.getLength() > 0){
-                return children.item(0).getTextContent();
+    // ok:find-sql-string-concatenation
+    protected void ok(String foo) throws ServletException, IOException {
+        String query = "select foo from bar where ? limit 1";
+        Session session = this.sessionFactory.openSession();
+        try {
+            PreparedStatement ps = session.connection().prepareStatement(query);
+            ps.setString(1,foo);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                return rs.getInt("foo");
             }
+        } catch (SQLException e) {
+            logger.error("Error!", e);
+        } finally {
+            session.close();
         }
-        LOG.debug("no text content of tag with name: {}", tagName);
-        return null;
     }
-    
 }
